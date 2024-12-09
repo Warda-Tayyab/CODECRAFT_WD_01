14d059d78215c50a5ed6c9a23498f87244299f65
Warda Tayyab <wardat057@gmail.com>
2024-12-08 18:45:54 -0800

    commit message

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/CodeCRAFT_WD_01",
+  base: "/CODECRAFT_WD_01",
   plugins: [react()],
 })
